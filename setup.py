a419c4164f0941ba4b1cd83d615812a65eb21cad
Javier <mendezalvarojavier1@gmail.com>
2026-03-27 14:34:55 -0600

    se agregan nuevos archivos

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Javier",                                # Tu nombre
+    author_email="mendezalvarojavier1@gmail.com",   # Tu correo electrónico
+    url="https://github.com/AlvaroJavier1/quintoRepo",     # URL del proyecto
+)
